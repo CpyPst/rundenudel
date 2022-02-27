a9de680ce084b77137c52847216491f56125d65a
Jonas Weckschmied <jonas@ib-weckschmied.de>
2022-02-27 22:09:45 +0100

    fix shit

--- rundenudel/app.py
+++ rundenudel/app.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 
-import email
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, Response
 import os
 import re
 from flask_wtf import FlaskForm
-from wtforms.fields import (
-    StringField,
-    EmailField,
-    IntegerField,
-    DateField,
-    BooleanField,
-)
-from wtforms.validators import Length, DataRequired, regexp, NumberRange, Email
-from rundenudel import Accomodation, Host
+from wtforms import StringField, EmailField, IntegerField, DateField, BooleanField
+from wtforms.validators import Length, DataRequired, Regexp, NumberRange, Email
+from flask_sqlalchemy import SQLAlchemy
+from rundenudel import Host, Accomodation
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
@@ -21,42 +15,48 @@ app.secret_key = os.getenv("FLASK_KEY")
 db = SQLAlchemy(app)
 
 
+def checked(form, field):
+    if not field.data:
+        raise ValidationError("Checkbox must be checked.")
+
+
 class OfferForm(FlaskForm):
     firstname = StringField("firstname", [Length(max=45), DataRequired()])
     lastname = StringField("lastname", [Length(max=45), DataRequired()])
     phone = StringField(
         "phone",
-        [DataRequired(), regexp("(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))")],
+        [DataRequired(), Regexp("(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))")],
     )
     email = EmailField("email", [DataRequired(), Email()])
-    startdate = DateField("startdate", [DataRequired()], format="%d.%M.%Y")
-    enddate = DateField("startdate", [DataRequired()], format="%d.%M.%Y")
+    startdate = DateField("startdate", [DataRequired()], format="%Y-%m-%d")
+    enddate = DateField("enddate", [DataRequired()], format="%Y-%m-%d")
     capacity = IntegerField("capacity", [DataRequired(), NumberRange(min=1, max=1000)])
     kidfriendly = BooleanField("kidfriendly")
     petfriendly = BooleanField("petfriendly")
     street = StringField("street", [DataRequired(), Length(max=45)])
-    zipcode = StringField("zipcode", [DataRequired(), regexp("^[0-9]{5}\b")])
+    zipcode = StringField(
+        "zipcode", [DataRequired(), Regexp(r"^[0-9]{5}"), Length(max=5)]
+    )
     aptnumber = StringField(
-        "aptnumber", [DataRequired(), regexp("^[0-9]{1,3}[a-zA-Z]?\b")]
+        "aptnumber", [DataRequired(), Regexp(r"^[0-9]{1,3}[a-zA-Z]?\b")]
     )
     city = StringField("city", [DataRequired(), Length(max=45)])
     dsgvo = BooleanField("dsgvo", [DataRequired(), checked])
+
 
 @app.route("/")
 def home():
     return redirect("/offer")
 
+
 @app.route("/offer/", methods=["GET", "POST"])
 def offer_form():
-    return render_template("offer.html")
-
-
-@app.route("/submit-offer", methods=["POST"])
-def process_offer():
     form = OfferForm()
     if form.validate_on_submit():
         # DB OPERATION
-        return redirect("/confirmation")
+        return redirect("/confirmation/")
+    else:
+        return render_template("offer.html", form=form)
 
 
 def handle_offer(form):
@@ -79,7 +79,6 @@ def handle_offer(form):
         db.session.commit()
     except Exception as e:
         print(e)
-
 
 
 def create_host(firstname, lastname, email, phone):
