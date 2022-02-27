005556d30c66058f600c5592af3d22bbf98d774e
Tom Wolf <tomwolf521@gmail.com>
2022-02-27 20:04:36 +0100

    input validation versuch 1

--- rundenudel/app.py
+++ rundenudel/app.py
@@ -3,10 +3,30 @@
 from flask import Flask, render_template, request
 import os
 import re
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, EmailField, IntegerField, DateField, BooleanField
+from wtforms.validators import Length, DataRequired, regexp, NumberRange, Email
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
 db = SQLAlchemy(app)
+
+class OfferForm(FlaskForm):
+    firstname = StringField("firstname", [Length(max=45), DataRequired()])
+    lastname = StringField("lastname", [Length(max=45), DataRequired()])
+    phone = StringField("phone", [DataRequired(), regexp("(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))")])
+    email = EmailField("email", [DataRequired(), Email()])
+    startdate = DateField("startdate", [DataRequired()], format='%d.%M.%Y')
+    enddate = DateField("startdate", [DataRequired()], format='%d.%M.%Y')
+    capacity = IntegerField("capacity", [DataRequired(), NumberRange(min=1, max=1000)]) 
+    kidfriendly = BooleanField("kidfriendly") 
+    petfriendly = BooleanField("petfriendly")
+    street = StringField("street", [DataRequired(), Length(max=45)])
+    zipcode = StringField("zipcode", [DataRequired(), regexp("^[0-9]{5}\b")])
+    aptnumber = StringField("aptnumber", [DataRequired(), regexp("^[0-9]{1,3}[a-zA-Z]?\b")])
+    city = StringField("city", [DataRequired(), Length(max=45)])
+
+
 
 @app.route("/")
 def home():
@@ -17,41 +37,12 @@ def home():
 def offer_form():
     return render_template("offer.html")
 
-@app.route("/offer-sent", methods=["POST"])
+@app.route("/submit-offer", methods=["POST"])
 def process_offer():
-    error = True
-    if request.method == "POST":
-        if "firstname" in request.form:
-            error = False
-        if "lastname" in request.form:
-            error = False
-        if "phone" in request.form:
-            phone_regex = re.compile("(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))")
-            if phone_regex.match(request.form["phone"]) is not None:
-                error = False
-        if "email" in request.form:
-            error = False
-        if "startdate" in request.form:
-            error = False
-        if "enddate" in request.form:
-            error = False
-        if "capacity" in request.form:
-            error = False
-        if "kidfriendly" in request.form:
-            error = False
-        if "petfriendly" in request.form:
-            error = False
-        if "street" in request.form:
-            error = False
-        if "zip" in request.form:
-            error = False
-        if "aptnumber" in request.form:
-            error = False
-        if "city" in request.form:
-            error = False
-        
-        if error: 
-            return "ERROR PAGE" # todo: render template with error message
+    form = OfferForm()
+    if form.validate_on_submit():
+        # DB OPERATION
+        return redirect("/confirmation")
         
 
 
